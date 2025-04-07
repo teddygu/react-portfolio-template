d0e00efa5d35e07ee345f55f3a54ef96731e4a24
teddygu <zinangu@gmail.com>
2025-04-07 16:31:21 -0500

    rerouting mail button

--- components/Header/index.js
+++ components/Header/index.js
@@ -145,7 +145,7 @@ const Header = ({ handleWorkScroll, handleAboutScroll, isBlog }) => {
               </Button>
             )}
 
-            <Button onClick={() => window.open("mailto:hello@chetanverma.com")}>
+            <Button onClick={() => window.open("mailto:zinanggu@gmail.com")}>
               Contact
             </Button>
             {mounted && theme && data.darkMode && (
@@ -174,7 +174,7 @@ const Header = ({ handleWorkScroll, handleAboutScroll, isBlog }) => {
               </Button>
             )}
 
-            <Button onClick={() => window.open("mailto:hello@chetanverma.com")}>
+            <Button onClick={() => window.open("mailto:zinangu@gmail.com")}>
               Contact
             </Button>
 
